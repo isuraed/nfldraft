5892124aef35c1b609221cb8dcc5e8c7c0275d70
Isura Edirisinghe <isurae@gmail.com>
2013-11-26 06:22:46 -0500

    Add draftpick model.

--- draft/app/models.py
+++ draft/app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Position(models.Model):
     name = models.CharField(max_length=255)
 
+
 class College(models.Model):
     name = models.CharField(max_length=255)
+
 
 class Player(models.Model):
     name = models.CharField(max_length=255)
@@ -13,7 +16,16 @@ class Player(models.Model):
     college = models.ForeignKey(College, null=True)
     grade = models.IntegerField()
 
+
 class Team(models.Model):
     name = models.CharField(max_length=255)
+
+
+class DraftPick(models.Model):
+    round = models.IntegerField()
+    pick = models.IntegerField()
+    team = models.ForeignKey(Team)
+    player = models.ForeignKey(Player, null=True)
+
 
 
