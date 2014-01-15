2aa7fccd69413c65a8b9cf1cb134f4be29c1844f
Isura Edirisinghe <isurae@gmail.com>
2014-01-14 21:38:34 -0500

    Refactor home.html to use template inheritence.

--- draft/app/views.py
+++ draft/app/views.py
@@ -6,5 +6,5 @@ def home(request):
     draft_picks = DraftPick.objects.filter(round=1)
     top_players = Player.objects.all().order_by('-grade')[:25]
     context = {'draft_order': draft_picks, 'top_players': top_players}
-    return render(request, 'home.html', context)
+    return render(request, 'base_pre.html', context)
 
