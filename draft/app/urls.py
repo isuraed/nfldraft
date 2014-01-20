f2bb8c52bd52ce02d5856c03b7906d49a18f6f1f
Isura Edirisinghe <isurae@gmail.com>
2014-01-19 22:43:24 -0500

    Add results template and fill with initial data.

--- draft/app/urls.py
+++ draft/app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.pre, name='pre'),
+    url(r'^results/$', views.results, name='results'),
+    url(r'^results/by-round/(\d{1})/$', views.results, name='results'),
 )
--- draft/app/views.py
+++ draft/app/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from app.models import DraftPick, Player
 
 
-def home(request):
+def pre(request):
     draft_picks = DraftPick.objects.filter(round=1)
     top_players = Player.objects.all().order_by('-grade')[:25]
     context = {'draft_order': draft_picks, 'top_players': top_players}
     return render(request, 'base_pre.html', context)
 
+def results(request, round_number="1"):
+    draft_picks = DraftPick.objects.filter(round=int(round_number))
+    context = {'draft_picks': draft_picks}
+    return render(request, 'base_results.html', context)
